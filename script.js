3672a03a36ad7145a80b800441ab7788fe2c8200
mograph <sljward@outlook.com>
2022-05-18 17:33:41 -0400

    5 characters

--- script.js
+++ script.js
@@ -5,7 +5,8 @@ const config = {
   //src: 'C:/Users/sljwa/OneDrive/Desktop/Reddit/10 Million Commerical/Sprite_Sheet_AME/Atlas_30.png',
   //src: 'https://github.com/mograph/Characters_Walking/blob/main/src/Atlas30.png',
   //src: 'C:/Users/sljwa/OneDrive/Documents/Reddit_Code/Characters_Walking/src/Atlas30.png',
-  src: 'src/Atlas30.png',
+  //src: 'src/Atlas30.png',
+  src: 'src/Atlas5.png',
   rows: 15,
   cols: 7 };
 
